cfe78452c2b8c7d4b51cfdb45ecb7b3fb71238d7
YulaySC <yulyssc@gmail.com>
2014-10-12 14:16:06 -0600

    Uniendo1

--- Uniendo/Inicial.py
+++ Uniendo/Inicial.py
@@ -11,7 +11,6 @@ def mQuit():
         return
 
 
-
 ##""" Llamar a las funciones (lexico, sintactico) de Tkinter a dll """
 ##
 ##def llamarFunciones():
@@ -31,43 +30,25 @@ def mQuit():
 mGui = Tk() #master
 mGui.config(bg="white") #color del fondo de la ventana
 mGui.geometry('500x500+455+100')#tamaño de la ventana
-mGui.title('Juego ~SWI Prolog~')#Nombre de la ventana
-mlabel = Label(mGui,text ='SNAKE',fg='red', bg='black').pack() #TÍTULO 
+mGui.title('~SWI Prolog~')#Nombre de la ventana
+mlabel = Label(mGui,text ='SWI Prolog',fg='red', bg='green').pack() #TÍTULO 
 
-##fondo=PhotoImage(file="buho.gif")
-##fondoLabel=Label(mGui,image=fondo,bd=2,bg="#000000")
-##fondoLabel.image=fondo
-##fondoLabel.pack()
-##
-##""" definir modos de juego """ ##""" Botones """##
-##
-##""" Botón de Modo Juego """
-##mbuttonModoJuego = Button(mGui,text='Modo juego',fg = 'white', bg = 'blue').place(x=70,y=20)
-##
-##tamaño = Label(mGui,text ='Dimensiones de la ventana deseada:',fg='white',bg='green').place(x=20,y=50)
-##recuerde = Label(mGui,text ='Recuerde:',fg='white',bg='green').place(x=70,y=70)
-##Largo = Label(mGui,text='Largo: 15 -- 50',fg='white',bg='green').place(x=70,y=90)
-##Ancho = Label(mGui,text='Ancho: 15 -- 25',fg='white',bg='green').place(x=70,y=110)
-##
-##Medida1=Entry(mGui)
-##Medida1.place(x=50,y=130)
-##
-##Medida2=Entry(mGui)
-##Medida2.place(x=50,y=150)
-##mbuttonOK = Button(mGui,text='OK',command= validar, fg = 'white', bg = 'blue').place(x=50,y=170)
-##
-##
-##
-##""" Construcción de la barra de menús """
-##menubar=Menu(mGui)#Barra de menú
-##
-##filemenu = Menu(menubar,tearoff=0)
-##filemenu.add_command(label = "Exit",command = mQuit)
-##menubar.add_cascade(label = "File",menu=filemenu)
-##
-##mGui.config(menu=menubar)
-##
-##WIDTH=Medida1.get()
-##HEIGHT=Medida2.get()
+fondo=PhotoImage(file="Buho12.gif")
+fondoLabel=Label(mGui,image=fondo,bd=2,bg="#000000")
+fondoLabel.image=fondo
+fondoLabel.pack()
 
-mGui.mainloop()
+
+
+""" Construcción de la barra de menús """
+menubar=Menu(mGui)#Barra de menú
+
+filemenu = Menu(menubar,tearoff=0)
+filemenu.add_command(label = "Exit",command = mQuit)
+menubar.add_cascade(label = "File",menu=filemenu)
+
+mGui.config(menu=menubar)
+
+
+
+mGui.mainloop() #para que funcione en Windows
